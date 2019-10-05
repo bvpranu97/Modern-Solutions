d8c63699cbaf474006f402d1514ac62f4ba53520
bvpranu97 <49627284+bvpranu97@users.noreply.github.com>
2019-10-05 13:50:11 +0530

    Add files via upload

---
+++ Camel case.cpp
@@ -0,0 +1,41 @@
+/*
+BY: Coding Cat
+Domain : Illegal Coding
+Link: https://www.hackerrank.com/challenges/camelcase/problem
+*/
+
+
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// Complete the camelcase function below.
+int camelcase(string s) {
+ int w=1;
+ for ( int i=0 ; i<s.size() ; i++ )
+ {
+     if( s[i] <='Z' && s[i]>='A' )
+     w=w+1;
+
+ } 
+ return w;
+
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    string s;
+    getline(cin, s);
+
+    int result = camelcase(s);
+
+    fout << result << "\n";
+
+    fout.close();
+
+    return 0;
+}
+
